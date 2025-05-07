5b76ae34d2c26557e61440856616d2ea7a35dd94
Todd Rinaldo <toddr@cpan.org>
2025-05-07 16:05:17 +0000

    Bump changelog for release of 0.35 to CPAN

--- RSA.pm
+++ RSA.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Carp;    # Removing carp will break the XS code.
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 use XSLoader;
 XSLoader::load 'Crypt::OpenSSL::RSA', $VERSION;
